46f9b351c7e4bb21f93840a73d32565e30f8de9a
raz-ezra <raz.ezra@sundaysky.com>
2019-02-15 01:17:06 +0200

    work with a bin commands

--- src/concatDebugFiles.js
+++ src/concatDebugFiles.js
@@ -2,6 +2,7 @@ const fs = require("fs-extra");
 const path = require("path");
 const globby = require("globby");
 const DebugFiles = require("./DebugFiles");
+const chalk = require("chalk");
 
 module.exports = workingDir => {
   const debugFilePaths = globby.sync(path.join(workingDir, "**", "Debug.txt"));
@@ -12,9 +13,22 @@ module.exports = workingDir => {
     debugFiles.addFile(file);
   });
 
+  // Do the relevant transformations
   debugFiles.shift();
+
   const masterDebugContents = debugFiles.mergeToText();
 
   const masterDebugPath = path.join(workingDir, "masterDebug.txt");
+
+  if (fs.existsSync(masterDebugPath)) {
+    console.log("There is already a file in:");
+    console.log(chalk.magenta(masterDebugPath));
+    console.log("Overriding...");
+    console.log();
+  }
+
+  console.log(chalk.green("created master file in:"));
+  console.log(chalk.cyan(masterDebugPath));
+
   fs.outputFileSync(masterDebugPath, masterDebugContents);
 };
